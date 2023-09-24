76916ddc55aaebb71ba6b47f45c1c21b42ea00ce
Casey Baggz <caseybaggz@gmail.com>
2023-09-24 16:34:16 -0500

    initial setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+const ERROR = 2
+const WARN = 1
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:solid/typescript',
+    // this must be last
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [],
+  rules: {},
+}
